6338ff919427eb12797c5df97b5a4360d292ba48
teplatformdesign <62630846+teplatformdesign@users.noreply.github.com>
2020-08-14 17:41:25 -0700

    trying to make the plugins work

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// baguette box instructions to run the lightbox
+window.addEventListener('load', function () {
+ baguetteBox.run('.gallery');
+});
+
+const search = new Filter('search', 'data-caption');
